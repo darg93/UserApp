405bc8bedd41409d9aad62ee017f73075f7a6b2b
darg93 <daniel.riano93@gmail.com>
2021-10-01 12:04:07 -0500

    debugging V1

--- Backend/src/main/java/com/octans/models/Role.java
+++ Backend/src/main/java/com/octans/models/Role.java
@@ -15,5 +15,19 @@ public class Role {
 
     @Column(name = "NOMBRE")
     private String nombre;
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
---
+++ Backend/src/main/java/com/octans/models/User.java
@@ -0,0 +1,55 @@
+package com.octans.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USUARIO")
+public class User {
+
+    @Id
+    @Column(name = "ID_USUARIO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private Integer id_rol;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private Character activo;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(Integer id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Character getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Character activo) {
+        this.activo = activo;
+    }
+
+}
